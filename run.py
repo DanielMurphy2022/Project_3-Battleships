4be579679d19cdd1abe093f6d4ed0a1540ca4f5c
Daniel Murphhy <danomurphy@live.ie>
2024-05-15 22:40:09 +0000

    added if/else statements

--- run.py
+++ run.py
@@ -62,3 +62,42 @@ class BattleshipsGame:
         ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)]  # Create ships of different sizes
         for ship in ships:
             self.board.place_ship(ship)  # Place ships on the game board
+
+    def player_turn(self):
+        print("Your Turn!")
+        self.player_board.print_board()  # Print player's board
+        while True:
+            try:
+                guess_row = int(input("Guess Row (0 - {}): ".format(self.size - 1)))  # Get player's guess for row
+                guess_col = int(input("Guess Col (0 - {}): ".format(self.size - 1)))  # Get player's guess for column
+
+                if guess_row < 0 or guess_row >= self.size or guess_col < 0 or guess_col >= self.size:
+                    print("Oops, that's off the grid. Try again.")  # Check if guess is out of bounds
+                    continue
+
+                if self.computer_board.grid[guess_row][guess_col] == 'X' or self.computer_board.grid[guess_row][guess_col] == 'H':
+                    print("You've already guessed that. Try again.")  # Check if guess has already been made
+                    continue
+
+                if self.computer_board.grid[guess_row][guess_col] == 'S':
+                    print("Congratulations! You hit a ship!")  # Check if player's guess hit a ship
+                    self.computer_board.grid[guess_row][guess_col] = 'H'
+                    for row, col, direction, ship in self.computer_board.ships:
+                        if direction == 'horizontal':
+                            if row == guess_row and col <= guess_col < col + ship.size:
+                                ship.hits += 1
+                                if ship.is_sunk():
+                                    print("You've sunk a ship!")  # Check if a ship has been sunk
+                                break
+                        else:
+                            if col == guess_col and row <= guess_row < row + ship.size:
+                                ship.hits += 1
+                                if ship.is_sunk():
+                                    print("You've sunk a ship!")  # Check if a ship has been sunk
+                                break
+                else:
+                    print("You missed!")  # Player missed the ship
+                    self.computer_board.grid[guess_row][guess_col] = 'X'
+                break
+            except ValueError:
+                print("Please enter a valid number.")  # Handle invalid input
